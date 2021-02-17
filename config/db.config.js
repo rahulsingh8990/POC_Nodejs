458b8689e2ee550784b40fd835d537db43aa54fd
rahul singh <rahul.8990.singh@gmail.com>
2021-02-17 16:36:32 +0530

    pushed for issue

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: 'passwordhistorydb.c2jne8vaft6v.us-east-1.rds.amazonaws.com',
+    HOST: '',
     USER : "root",
-    PASSWORD: "password123",
+    PASSWORD: "",
     DB :"passwordhistorydb"
 } 
